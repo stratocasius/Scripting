992364c64061cc6dc118519a311058e8220c51fa
stratocasius <67420693+stratocasius@users.noreply.github.com>
2021-07-16 11:24:01 -0400

    Add files via upload

---
+++ ChromeBookmarks.ps1
@@ -0,0 +1,9 @@
+
+<enabled/>
+
+<data id='JL Bookmarks' value='[{"toplevel_name":"JL Bookmarks"},{"url":"https://workforcenow.adp.com/public/index.html","name":"ADP WorkForce"},{"url":"https://app.ca1.chromeriver.com/login/sso/saml?CompanyId=jacksonlewis.com","name":"Chrome River"},{"url":"dashboard","name":"Dashboard"},{"url":"www.jacksonlewis.com","name":"Jackson Lewis"},{"url":"remote.jacksonlewis.com","name":"JL Citrix"},{"url":"https://advance.lexis.com/?federationidp=QFCFCW55551","name":"Lexis Advance"},{"url":"https://vault.netvoyage.com","name":"Netdocuments"},{"url":"https://nbi.jacksonlewis.com/Open/Intake/Default.aspx","name":"New Business Intake (NBI)"},{"url":"https://jacksonlewis.service-now.com/","name":"JL Service Center"},{"url":"http://psship","name":"PSSHIP"},{"url":"https://home.jacksonlewis.com/Pages/KMLegalResourceCenter.aspx","name":"KM Legal Resource Center"},{"url":"https://securetransfer.jacksonlewis.com/bds/Login.do","name":"Biscom"},{"url":"https://www.concursolutions.com/nui/signin","name":"Concur"}]}'/>
+
+
+
+
+{"name":"Email services","children":[{"url":"gmail.com","name":"Gmail"},{"url":"outlook.com","name":"Outlook"}]}]'/>
